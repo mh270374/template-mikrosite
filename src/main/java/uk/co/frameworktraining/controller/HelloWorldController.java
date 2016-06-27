26a351df81fc7c21200f5f7072a834d12b4ae1fa
Michael Harrison <michael.harrison@insight.com>
2016-06-27 13:55:44 +0100

    first commit

---
+++ src/main/java/uk/co/frameworktraining/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package uk.co.frameworktraining.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Success! The Framework Training template microservices is up and running";
+	}
+
+	@RequestMapping("/boosh")
+	@ResponseBody
+	String boosh() { return "Test"; }
+}
